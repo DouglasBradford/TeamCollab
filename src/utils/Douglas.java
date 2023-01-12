7d2a9bc9d3c9716eb6274bf4f0dc2e006ce99882
douglasbradford <dcb281@gmail.com>
2023-01-12 18:54:48 -0500

    Added requested updates to Douglas file

--- src/utils/Douglas.java
+++ src/utils/Douglas.java
@@ -1,11 +1,17 @@
 package utils;
 
 public class Douglas {
-    // Stuff
+    //Stuff
 
     //Stuff
 
     //lots of stuff
 
     //good stuff
+
+    //Even more lines of stuff
+
+    // lots of lines of stuff
+
+    //Huge amounts of stuff
 }
